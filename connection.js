82767420162c3999b1045508238723e5d5870fab
Jocelin-La-Roch <60178232+Jocelin-La-Roch@users.noreply.github.com>
2021-06-18 01:36:18 +0100

    set ssl

--- connection.js
+++ connection.js
@@ -17,7 +17,12 @@ const sequelize = new Sequelize(
     '17adb6fff84cd92d94c6a996b636c0570409ec4ef77a0dbbe2bddc7b2d84b3d2',
     {
         dialect: 'postgres',
-        host: 'ec2-23-23-128-222.compute-1.amazonaws.com'
+        host: 'ec2-23-23-128-222.compute-1.amazonaws.com',
+        ssl: true,
+        native: true,
+        dialectOptions: {
+            ssl: true
+        }
     }
 );
 
