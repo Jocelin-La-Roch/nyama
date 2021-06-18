1aeec75f7ff4d63466a42aab30c25890cee08500
Jocelin-La-Roch <60178232+Jocelin-La-Roch@users.noreply.github.com>
2021-06-18 01:03:05 +0100

    set database

--- connection.js
+++ connection.js
@@ -12,12 +12,12 @@ module.exports = client;*/
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'node-postgre',
-    'postgres',
-    'ICEstorm123',
+    'dd1lff6k53fppp',
+    'bjsqnyihfpacxm',
+    '17adb6fff84cd92d94c6a996b636c0570409ec4ef77a0dbbe2bddc7b2d84b3d2',
     {
         dialect: 'postgres',
-        host: 'localhost'
+        host: 'ec2-23-23-128-222.compute-1.amazonaws.com'
     }
 );
 
