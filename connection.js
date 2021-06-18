60078fcbffd8990a9081257ee7327b2a5628499f
Jocelin-La-Roch <60178232+Jocelin-La-Roch@users.noreply.github.com>
2021-06-18 01:43:41 +0100

    etst

--- connection.js
+++ connection.js
@@ -16,14 +16,15 @@ const sequelize = new Sequelize(
     'bjsqnyihfpacxm',
     '17adb6fff84cd92d94c6a996b636c0570409ec4ef77a0dbbe2bddc7b2d84b3d2',
     {
-        dialect: 'postgres',
-        host: 'ec2-23-23-128-222.compute-1.amazonaws.com',
-        ssl: true,
-        native: true,
-        dialectOptions: {
-            ssl: true
+        "production": {
+            "dialect":"postgres",
+            "host": 'ec2-23-23-128-222.compute-1.amazonaws.com',
+            "ssl": true,
+            "dialectOptions": {
+                "ssl": true
+            }
         }
-    }
+    },
 );
 
 module.exports = sequelize;
