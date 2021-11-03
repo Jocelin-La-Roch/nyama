e2d181eaa35e2888707cbc6fdea670efab5ffeb3
Jocelin-La-Roch <jocelinlaroch08@gmail.com>
2021-11-04 00:43:05 +0100

    create food table

--- app/models/Food.js
+++ app/models/Food.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../utils/database');
 
-const Food = sequelize.define('product', {
+const Food = sequelize.define('food', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
