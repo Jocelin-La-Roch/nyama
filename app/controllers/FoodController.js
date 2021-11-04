4e82570b7d31e63078729ad9ca960651ec382537
Jocelin-La-Roch <jocelinlaroch08@gmail.com>
2021-11-04 01:23:13 +0100

    fix issue

--- app/controllers/FoodController.js
+++ app/controllers/FoodController.js
@@ -1,8 +1,8 @@
 const Food = require('../models/Food');
 
 exports.addFood = (req, res, next) => {
-  console.log("title: ", req.body.title, "imageUrl: ", req.body.imageUrl, "price: ", req.body.price, "description: ", req.body.description)
-    const name = req.body.title;
+  console.log("title: ", req.body.name, "imageUrl: ", req.body.imageUrl, "price: ", req.body.price, "description: ", req.body.description)
+    const name = req.body.name;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
